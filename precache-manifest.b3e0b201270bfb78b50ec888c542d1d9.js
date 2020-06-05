709a295bbafcb54885fccae4530ca1d1d87461da
adnananwar97 <32822063+adnananwar97@users.noreply.github.com>
2020-06-05 23:53:12 +0500

    Updates

---
+++ precache-manifest.b3e0b201270bfb78b50ec888c542d1d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b62cde1f2871213a736e91ebb3e394",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "25b80d753ade0b3bc5ea",
+    "url": "/monsters-rolodex/static/css/main.fa4a1d26.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b80d753ade0b3bc5ea",
+    "url": "/monsters-rolodex/static/js/main.901468aa.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
